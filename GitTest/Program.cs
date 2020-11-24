74dc959c557d2455140727f50d469a4548d45e5c
Vagner Santos <vgr.c.santos@hotmail.com>
2020-11-24 14:41:25 -0300

    Desejando um bom dia

--- GitTest/Program.cs
+++ GitTest/Program.cs
@@ -6,9 +6,9 @@ namespace GitTest
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Console.WriteLine("Bom dia");
+            Console.WriteLine("Hello World!");   
             Console.WriteLine("Como voce esta hoje?");
+            Console.WriteLine("Tenha um bom dia!");
 
         }
     }
